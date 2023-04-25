33c4efdc92effed1ab780f2acd96a74c896e178d
JunJie Qu ( Rex ) <junjiequ123@gmail.com>
2023-04-25 16:01:27 +0000

    Sync LeetCode submission - Is Subsequence (javascript)

---
+++ my-folder/problems/is_subsequence/solution.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function(s, t) {
+    let result = 0
+
+    for(let i = 0;i<t.length;i++){
+        if(s[result] === t[i]){
+            result++
+        }
+    }
+
+    return result === s.length
+
+};
