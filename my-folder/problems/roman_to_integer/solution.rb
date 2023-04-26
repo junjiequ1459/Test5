2c6da7b270e7b45cf810cb23f33eff99355c92c4
JunJie Qu ( Rex ) <junjiequ123@gmail.com>
2023-04-26 23:34:01 +0000

    Sync LeetCode submission - Roman to Integer (ruby)

--- my-folder/problems/roman_to_integer/solution.rb
+++ my-folder/problems/roman_to_integer/solution.rb
@@ -1,15 +1,15 @@
 def roman_to_int(s)
   roman_key = {"I"=>1,"V"=>5,"X"=>10,"L"=>50,"C"=>100,"D"=>500,"M"=>1000}
   result = 0
-  i = 0
-  while i < s.length
-    curr_value = roman_key[s[i]]
-    if i == s.length - 1 || roman_key[s[i+1]] <= roman_key[s[i]]
-      result += curr_value
+  prev_value = 0
+  s.each_char do |c|
+    curr_value = roman_key[c]
+    if curr_value > prev_value
+      result -= prev_value
     else
-      result -= curr_value
+      result += prev_value
     end
-    i += 1
+    prev_value = curr_value
   end
-  result
+  result += prev_value
 end
