dc12a03f066e6853cece1b8787a58c85f5d3a8e7
JunJie Qu ( Rex ) <junjiequ123@gmail.com>
2023-05-03 23:18:29 +0000

    Sync LeetCode submission - Valid Sudoku (javascript)

--- my-folder/problems/valid_sudoku/solution.js
+++ my-folder/problems/valid_sudoku/solution.js
@@ -1,30 +1,38 @@
 function isValidSudoku(board) {
-  for (let i = 0; i < 9; i++) {
-    const row = [];
-    const column = [];
-    const subBox = [];
 
-    for (let j = 0; j < 9; j++) {
-      // check row
-      if (board[i][j] !== "." && row.includes(board[i][j])) {
-        return false;
-      }
-      row.push(board[i][j]);
+    for(let i = 0;i<board.length;i++){
+        let row = []
+        let column = []
 
-      // check column
-      if (board[j][i] !== "." && column.includes(board[j][i])) {
-        return false;
-      }
-      column.push(board[j][i]);
+        for(j=0;j<board.length;j++){
+            if (board[i][j] !== "." && row.includes(board[i][j])){
+                return false
+            }
+            if (board[j][i] !== "." && column.includes(board[j][i])){
+                return false
+            }
+            row.push(board[i][j])
+            column.push(board[j][i])
 
-      // check sub-box
-      const rowIndex = 3 * Math.floor(i / 3) + Math.floor(j / 3);
-      const colIndex = 3 * (i % 3) + (j % 3);
-      if (board[rowIndex][colIndex] !== "." && subBox.includes(board[rowIndex][colIndex])) {
-        return false;
+
+        }
+    }
+    for (let i = 0; i < 9; i += 3) {
+    for (let j = 0; j < 9; j += 3) {
+      let subBoxSet = new Set();
+      for (let row = i; row < i + 3; row++) {
+        for (let col = j; col < j + 3; col++) {
+          if (board[row][col] !== '.') {
+            if (subBoxSet.has(board[row][col])) {
+              return false;
+            } else {
+              subBoxSet.add(board[row][col]);
+            }
+          }
+        }
       }
-      subBox.push(board[rowIndex][colIndex]);
     }
   }
-  return true;
+  return true
+
 }
