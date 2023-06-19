d9128b82c46efda09074072147ebc4b1016f42d3
JunJie Qu ( Rex ) <junjiequ123@gmail.com>
2023-06-19 16:20:09 +0000

    Sync LeetCode submission - Top K Frequent Elements (ruby)

--- my-folder/problems/top_k_frequent_elements/solution.rb
+++ my-folder/problems/top_k_frequent_elements/solution.rb
@@ -2,27 +2,19 @@
 # @param {Integer} k
 # @return {Integer[]}
 def top_k_frequent(nums, k)
-  counts = {}
-  nums.each do |num|
-    if counts[num]
-      counts[num] += 1
-    else
-      counts[num] = 1
-    end
-  end
+    frequency = Hash.new(0)
+    nums.each {|num| frequency[num] += 1}
 
-  result = []
-  max_count = counts.values.max
-  while k > 0 && max_count > 0
-    counts.each do |num, count|
-      if count == max_count
-        result << num
-        k -= 1
-        break if k == 0
-      end
-    end
-    max_count -= 1
-  end
+    bucket = Array.new(nums.length + 1) {[]}
 
-  result
+    frequency.each do |key,value|
+    bucket[value] << key
+    end
+
+    result = []
+    (bucket.length - 1).downto(0) do |i|
+    result.concat(bucket[i])
+    break if result.length >= k
+    end
+    result
 end
