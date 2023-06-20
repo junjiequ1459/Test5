1fb34c2bad90acc6f31af91c65f33367e47f61d3
JunJie Qu ( Rex ) <junjiequ123@gmail.com>
2023-06-20 15:07:04 +0000

    Sync LeetCode submission - Top K Frequent Elements (ruby)

--- my-folder/problems/top_k_frequent_elements/solution.rb
+++ my-folder/problems/top_k_frequent_elements/solution.rb
@@ -1,8 +1,13 @@
 # @param {Integer[]} nums
 # @param {Integer} k
 # @return {Integer[]}
+
+
+
 def top_k_frequent(nums, k)
+
     frequency = Hash.new(0)
+
     nums.each {|num| frequency[num] += 1}
 
     bucket = Array.new(nums.length + 1) {[]}
